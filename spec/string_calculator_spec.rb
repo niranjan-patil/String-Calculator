3ee8435f63d0ecc6cdb458d8025418fc34b07166
Niranjan Patil <niranjanpatil582@gmail.com>
2025-08-30 21:21:03 +0530

    test: Add spec for handling newline as a delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -29,5 +29,12 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
       end
     end
+
+    context 'with new lines between numbers' do
+      it 'treats newlines as delimiters' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+        expect(StringCalculator.add("1,2\n3")).to eq(6)
+      end
+    end
   end
 end
